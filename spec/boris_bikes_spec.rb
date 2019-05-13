a1f70968548f5f84e68ca4223422cdfad7702b37
Student <student@MAKERSs-MacBook-Pro-10.local>
2019-05-13 15:48:13 +0100

    Written vague test for docking station

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,3 +1,9 @@
-require './library/boris_bikes'
+require './library/docking_station'
 describe DockingStation do
+  #arrange
+  docking_station = DockingStation.new
+  #act
+  docking_station.release_bike
+  #assertion
+  expect(docking_station).not_to include(bike)
 end
