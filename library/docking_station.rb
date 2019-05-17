67f01aa20b460ebee52e975f478577a24567a434
Student <student@MAKERSs-MacBook-Pro-10.local>
2019-05-17 13:41:51 +0100

    Added new private methods full? and empty? to code to use Single Responsibility Principle

--- library/docking_station.rb
+++ library/docking_station.rb
@@ -6,14 +6,23 @@ attr_reader :bikes
   end
 
   def release_bike
-    raise "No bike here" if @bikes.empty?
+    raise "No bike here" if docking_station.empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Cannot dock, capacity full" if @bikes.length >= 20
+    raise "Cannot dock, capacity full" if docking_station.full?
       @bikes.push(bike)
   end
 
+  private
+  def full?
+    @bikes.length >= 20 ? return true : return false
+  end
+
+  private
+  def empty?
+    @bikes.empty? ? return true : return false
+  end
 
 end
